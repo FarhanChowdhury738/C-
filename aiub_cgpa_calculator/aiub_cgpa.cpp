0f3cf17acf20314a56a76ea8e90cafc5271e715c
FarhanChowdhury <76951531+farhanchowdhury738@users.noreply.github.com>
2024-11-26 20:54:49 +0600

    Update aiub_cgpa.cpp
    cgpa_calculate

--- aiub_cgpa_calculator/aiub_cgpa.cpp
+++ aiub_cgpa_calculator/aiub_cgpa.cpp
@@ -1,250 +1,40 @@
 #include <iostream>
 using namespace std;
 
-int main () {
+int main() {
+    int n;
+    cout << "Enter the number of courses: ";
+    cin >> n;
 
-    string subject1, subject2, subject3, subject4, subject5, subject6;
-    int credit1, credit2, credit3, credit4, credit5, credit6;
-    int num;
-    float cgpa_s1, cgpa_s2, cgpa_s3, cgpa_s4, cgpa_s5, cgpa_s6;
+    float marks[n];
+    float totalGrade = 0.0;
+    int credit = 3;
 
-
-    //subject 1
-    cout<< "enter your Subject name: ";
-    cin>>subject1;
-    cout<< "How many credit: " ;
-    cin>>credit1;
-    cout<< "Subject mark: ";
-    cin>>num;
-    if (num<50){
-        cout<< "You're fail.";
-    } else if (num<60){
-        cout<< "Your grade D & cpga 2.25"<<endl;
-    }  else if (num<65){
-        cout<< "Your grade D+ & cgpa 2.50"<<endl;
-    } else if (num<70){
-        cout<< "Your grade C & cpga 2.75"<<endl;
-    }  else if (num<75){
-        cout<< "Your grade C+ & cgpa 3.00"<<endl;
-    } else if (num<80){
-        cout<< "Your grade B & cpga 3.25"<<endl;
-    }  else if (num<85){
-        cout<< "Your grade B+ & cgpa 3.50"<<endl;
-    } else if (num<90){
-        cout<< "Your grade A & cpga 3.75"<<endl;
-    }  else if (num<100){
-        cout<< "Your grade A+ & cgpa 4.00"<<endl;
-    } else {
-        cout<< "this num is invaild"<<endl;
+    for (int i = 0; i < n; ++i) {
+        cout << "Enter marks " << i + 1 << ": ";
+        cin >> marks[i];
+        if (marks[i] >= 90)
+            totalGrade += 4 * credit;
+        else if (marks[i] >= 85)
+            totalGrade += 3.75 * credit;
+        else if (marks[i] >= 80)
+            totalGrade += 3.5 * credit;
+        else if (marks[i] >= 75)
+            totalGrade += 3.25 * credit;
+        else if (marks[i] >= 70)
+            totalGrade += 3.0 * credit;
+        else if (marks[i] >= 65)
+            totalGrade += 2.75 * credit;
+        else if (marks[i] >= 60)
+            totalGrade += 2.5 * credit;
+        else if (marks[i] >= 50)
+            totalGrade += 2.25 * credit;
     }
-    cout<< "type cgpa: ";
-    cin>>cgpa_s1;
-    float cgpa_total_s1= cgpa_s1*credit1;
-    cout<< cgpa_total_s1<<endl;
 
-    // subject 2
-    cout<< "enter your Subject name: ";
-    cin>>subject2;
-    cout<< "How many credit: ";
-    cin>>credit2;
-    cout<< "Subject mark: ";
-    cin>>num;
-    if (num<50){
-        cout<< "You're fail.";
-    } else if (num<60){
-        cout<< "Your grade D & cpga 2.25"<<endl;
-    }  else if (num<65){
-        cout<< "Your grade D+ & cgpa 2.50"<<endl;
-    } else if (num<70){
-        cout<< "Your grade C & cpga 2.75"<<endl;
-    }  else if (num<75){
-        cout<< "Your grade C+ & cgpa 3.00"<<endl;
-    } else if (num<80){
-        cout<< "Your grade B & cpga 3.25"<<endl;
-    }  else if (num<85){
-        cout<< "Your grade B+ & cgpa 3.50"<<endl;
-    } else if (num<90){
-        cout<< "Your grade A & cpga 3.75"<<endl;
-    }  else if (num<100){
-        cout<< "Your grade A+ & cgpa 4.00"<<endl;
-    } else {
-        cout<< "this num is invaild"<<endl;
-    }
-    cout<< "type cgpa: ";
-    cin>>cgpa_s2;
-    float cgpa_total_s2= cgpa_s2*credit2;
-    cout<< cgpa_total_s2<<endl;
+    float totalCredits = n * credit;
+    float cgpa = totalGrade / totalCredits;
 
-    //subject 3
-    cout<< "enter your Subject name: ";
-    cin>>subject3;
-    cout<< "How many credit: ";
-    cin>>credit3;
-    cout<< "Subject mark: ";
-    cin>>num;
-    if (num<50){
-        cout<< "You're fail.";
-    } else if (num<60){
-        cout<< "Your grade D & cpga 2.25"<<endl;
-    }  else if (num<65){
-        cout<< "Your grade D+ & cgpa 2.50"<<endl;
-    } else if (num<70){
-        cout<< "Your grade C & cpga 2.75"<<endl;
-    }  else if (num<75){
-        cout<< "Your grade C+ & cgpa 3.00"<<endl;
-    } else if (num<80){
-        cout<< "Your grade B & cpga 3.25"<<endl;
-    }  else if (num<85){
-        cout<< "Your grade B+ & cgpa 3.50"<<endl;
-    } else if (num<90){
-        cout<< "Your grade A & cpga 3.75"<<endl;
-    }  else if (num<100){
-        cout<< "Your grade A+ & cgpa 4.00"<<endl;
-    } else {
-        cout<< "this num is invaild"<<endl;
-    }
-    cout<< "type cgpa: ";
-    cin>>cgpa_s3;
-    float cgpa_total_s3= cgpa_s3*credit3;
-    cout<< cgpa_total_s3<<endl;
+    cout << "Total CGPA: " << cgpa << endl;
 
-    //subject 4
-    cout<< "enter your Subject name: ";
-    cin>>subject4;
-    cout<< "How many credit: ";
-    cin>>credit4;
-    cout<< "Subject mark: ";
-    cin>>num;
-    if (num<50){
-        cout<< "You're fail.";
-    } else if (num<60){
-        cout<< "Your grade D & cpga 2.25"<<endl;
-    }  else if (num<65){
-        cout<< "Your grade D+ & cgpa 2.50"<<endl;
-    } else if (num<70){
-        cout<< "Your grade C & cpga 2.75"<<endl;
-    }  else if (num<75){
-        cout<< "Your grade C+ & cgpa 3.00"<<endl;
-    } else if (num<80){
-        cout<< "Your grade B & cpga 3.25"<<endl;
-    }  else if (num<85){
-        cout<< "Your grade B+ & cgpa 3.50"<<endl;
-    } else if (num<90){
-        cout<< "Your grade A & cpga 3.75"<<endl;
-    }  else if (num<100){
-        cout<< "Your grade A+ & cgpa 4.00"<<endl;
-    } else {
-        cout<< "this num is invaild"<<endl;
-    }
-    cout<< "type cgpa: ";
-    cin>>cgpa_s4;
-    float cgpa_total_s4= cgpa_s4*credit4;
-    cout<< cgpa_total_s4<<endl;
-
-    // subject 5
-    cout<< "enter your Subject name: ";
-    cin>>subject5;
-    cout<< "How many credit: ";
-    cin>>credit5;
-    cout<< "Subject mark: ";
-    cin>>num;
-    if (num<50){
-        cout<< "You're fail.";
-    } else if (num<60){
-        cout<< "Your grade D & cpga 2.25"<<endl;
-    }  else if (num<65){
-        cout<< "Your grade D+ & cgpa 2.50"<<endl;
-    } else if (num<70){
-        cout<< "Your grade C & cpga 2.75"<<endl;
-    }  else if (num<75){
-        cout<< "Your grade C+ & cgpa 3.00"<<endl;
-    } else if (num<80){
-        cout<< "Your grade B & cpga 3.25"<<endl;
-    }  else if (num<85){
-        cout<< "Your grade B+ & cgpa 3.50"<<endl;
-    } else if (num<90){
-        cout<< "Your grade A & cpga 3.75"<<endl;
-    }  else if (num<100){
-        cout<< "Your grade A+ & cgpa 4.00"<<endl;
-    } else {
-        cout<< "this num is invaild"<<endl;
-    }
-    cout<< "type cgpa: ";
-    cin>>cgpa_s5;
-    float cgpa_total_s5= cgpa_s5*credit5;
-    cout<< cgpa_total_s5<<endl;
-
-    // subject 6
-   /* cout<< "enter your Subject name: ";
-    cin>>subject6;
-    cout<< "How many credit: ";
-    cin>>credit6;
-    cout<< "Subject mark: ";
-    cin>>num;
-    if (num<50){
-        cout<< "You're fail.";
-    } else if (num<60){
-        cout<< "Your grade D & cpga 2.25"<<endl;
-    }  else if (num<65){
-        cout<< "Your grade D+ & cgpa 2.50"<<endl;
-    } else if (num<70){
-        cout<< "Your grade C & cpga 2.75"<<endl;
-    }  else if (num<75){
-        cout<< "Your grade C+ & cgpa 3.00"<<endl;
-    } else if (num<80){
-        cout<< "Your grade B & cpga 3.25"<<endl;
-    }  else if (num<85){
-        cout<< "Your grade B+ & cgpa 3.50"<<endl;
-    } else if (num<90){
-        cout<< "Your grade A & cpga 3.75"<<endl;
-    }  else if (num<100){
-        cout<< "Your grade A+ & cgpa 4.00"<<endl;
-    } else {
-        cout<< "this num is invaild"<<endl;
-    }
-    cout<< "type cgpa: ";
-    cin>>cgpa_s6;
-    float cgpa_total_s6= cgpa_s6*credit6;
-    cout<< cgpa_total_s6<<endl;
-*/
-
-    // total cgpa calculate
-    float cgpa_total_subject= cgpa_total_s1+cgpa_total_s2+cgpa_total_s3+cgpa_total_s4+cgpa_total_s5;
-    int total_credit= credit1+credit2+credit3+credit4+credit5;
-
-    double total_cgpa = cgpa_total_subject/total_credit;
-    cout<< "Your cgpa: "<<total_cgpa<<endl;
-
-
-
-        // Question
-        // sob if/else condition ki ekbar e kora jabe na?
-        // user theke ami chaile 2ta subject cgpa find korbo(sob somoy 5ta subject korbo na)
-
-
-
-
-   /* if (num<50){
-        cout<< "You're fail.";
-    } else if (num<60){
-        cout<< "Your grade D & cpga 2.25"<<endl;
-    }  else if (num<65){
-        cout<< "Your grade D+ & cgpa 2.50"<<endl;
-    } else if (num<70){
-        cout<< "Your grade C & cpga 2.75"<<endl;
-    }  else if (num<75){
-        cout<< "Your grade C+ & cgpa 3.00"<<endl;
-    } else if (num<80){
-        cout<< "Your grade B & cpga 3.25"<<endl;
-    }  else if (num<85){
-        cout<< "Your grade B+ & cgpa 3.50"<<endl;
-    } else if (num<90){
-        cout<< "Your grade A & cpga 3.75"<<endl;
-    }  else if (num<100){
-        cout<< "Your grade A+ & cgpa 4.00"<<endl;
-    } else {
-        cout<< "this num is invaild"<<endl;
-    }
-*/
-return 0;
+    return 0;
 }
